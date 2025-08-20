256cdfc1715b0ed969881d7c93aa5827c897c4e7
NKulbaka <nikita.kulbaka@mail.ru>
2025-08-21 01:11:53 +0300

    Initial commit

---
+++ bushunter/src/main/java/ru/kulbaka/bushunter/BushunterApplication.java
@@ -0,0 +1,13 @@
+package ru.kulbaka.bushunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BushunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BushunterApplication.class, args);
+	}
+
+}
